297933fdb3e087c37c1786f50ad5402b8f2a3ceb
Sooraj <sooraj@healthplotter.com>
2019-01-11 13:00:50 +0530

    API for hair loss

--- src/app/hairloss/hairloss.ts
+++ src/app/hairloss/hairloss.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {IonicPage} from "ionic-angular";
+import { HttpClient } from '@angular/common/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 
 @IonicPage()
@@ -9,7 +11,33 @@ import {IonicPage} from "ionic-angular";
 })
 export class hairLossPage {
   
-  constructor() {
+  apiUrl = 'http://localhost:3000';
+  formData: any;
+  postData: any;
+
+  constructor(public httpClient: HttpClient) {
+
+    this.formData = {}
 
   }
+
+  submitHairLoss() {
+    console.log(this.formData)
+
+    let urlSearchParams = new URLSearchParams();
+    urlSearchParams.append('assesmentname', "Hair Loss");
+
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    const requestOptions = new RequestOptions({ headers: headers });
+
+    this.httpClient.post(this.apiUrl+'/app/v1/assesment?'+ urlSearchParams.toString(), JSON.stringify(this.formData), requestOptions)
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+    });
+  }
+  
 }
